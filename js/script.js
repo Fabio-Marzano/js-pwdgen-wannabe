d25556483fd513f640169eec185547d3f25b87f1
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-25 18:01:30 +0200

    [UPD] insert-second-name-and-color

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 //CONSENTO INSERIMENTO DEL NOME DA PARTE DELL'UTENTE//
 let first_name = prompt('Inserisci il tuo nome');
 console.log(first_name);
+
+//CONSENTO INSERIMENTO DEL COGNOME DA PARTE DELL'UTENTE//
+let last_name = prompt('Inserisci il tuo cognome');
+console.log(last_name);
+
+//CONSENTO INSERIMENTO DEL COLORE DA PARTE DELL'UTENTE//
+let color = prompt('Inserisci il tuo colore preferito');
+console.log(color);
